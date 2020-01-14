17c7820a602359169b9006f85bc4aa2dc488c2e0
金龙浩 <jinlha@jiedaibao.com>
2020-01-14 16:10:22 +0800

    回溯

--- normal/39.py
+++ normal/39.py
@@ -17,3 +17,21 @@ class Solution:
             for i in choice:
                 self.backtrace(trace+[i], choice, S+i)
         
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        list_len = len(candidates)
+        if list_len == 0:
+            return []
+        candidates.sort()
+        result = []
+        def helper(i,tmp,target):
+            if target == 0:
+                result.append(tmp)
+                return
+            if i == list_len or target < candidates[i]:
+                return
+            helper(i,tmp+[candidates[i]],target-candidates[i])
+            helper(i+1,tmp,target)
+        helper(0,[],target)
+        return result
+        
