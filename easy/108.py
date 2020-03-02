e79add4b3890764e30b77b7e035e5d39733c658e
金龙浩 <jinlha@jiedaibao.com>
2020-03-02 12:34:07 +0800

    将有序数组转换为二叉搜索树

---
+++ easy/108.py
@@ -0,0 +1,43 @@
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:        
+        def helper(left, right):
+            if left > right:
+                return None
+            p = (left + right) // 2
+            root = TreeNode(nums[p])
+            root.left = helper(left, p - 1)
+            root.right = helper(p + 1, right)
+            return root
+        return helper(0, len(nums) - 1)
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:        
+        def helper(left, right):
+            if left > right:
+                return None
+            p = (left + right) // 2 
+            if (left + right) % 2:
+                p += 1 
+            root = TreeNode(nums[p])
+            root.left = helper(left, p - 1)
+            root.right = helper(p + 1, right)
+            return root
+        return helper(0, len(nums) - 1)
+
+
+from random import randint
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:        
+        def helper(left, right):
+            if left > right:
+                return None
+            p = (left + right) // 2 
+            if (left + right) % 2:
+                p += randint(0, 1) 
+            root = TreeNode(nums[p])
+            root.left = helper(left, p - 1)
+            root.right = helper(p + 1, right)
+            return root
+        return helper(0, len(nums) - 1)
+
