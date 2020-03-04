4e1d585adbdf4e28f19d8628cd93840af174fc52
金龙浩 <jinlha@jiedaibao.com>
2020-03-04 15:36:59 +0800

    验证二叉树的前序序列化

---
+++ normal/331.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        left, right = 0, 0
+        preorder = preorder.split(",")
+        if ["#"] == preorder:
+            return True
+        c = 0
+        for i in preorder:
+            if right < 0 or left < 0:
+                return False
+            if i != "#":
+                if left == 0 and right == 0:
+                    if c != 0:
+                        return False
+                    left += 1
+                    right += 1
+                    c += 1
+                elif left != 0:
+                    left += 1
+                else:
+                    right += 1
+            else:
+                if left != 0:
+                    left -= 1
+                else:
+                    right -= 1
+        return left == 0 and right == 0
+    
