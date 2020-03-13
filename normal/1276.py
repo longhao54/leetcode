54922e17f2828c9e1fa29701056dd4e2220892e0
longhao54 <493319173@qq.com>
2020-03-13 21:16:16 +0800

    数学 不浪费原料的汉堡制作方案

---
+++ normal/1276.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
+        if tomatoSlices % 2 != 0 or tomatoSlices < cheeseSlices * 2 or cheeseSlices * 4 < tomatoSlices:
+            return []
+        return [tomatoSlices // 2 - cheeseSlices, cheeseSlices * 2 - tomatoSlices // 2]
