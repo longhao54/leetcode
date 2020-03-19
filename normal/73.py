48723153192e82995407709dfa59de1343866bc9
金龙浩 <jinlha@jiedaibao.com>
2020-03-19 11:05:42 +0800

    矩阵置零

--- normal/73.py
+++ normal/73.py
@@ -3,8 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        line, row = [], []
-        for index, i in enumerate(matrix):
-            if 0 in i:
-                line.append(index)
-                 
+        c = set()
+        l = len(matrix[0])
+        for li,line in enumerate(matrix):
+            ck = False
+            for ri, v in enumerate(line):
+                if v == 0:
+                    c.add(ri)
+                    ck = True
+            if ck:
+                matrix[li] = [0] * l  
+        l1 = len(matrix)
+        for i in c:
+            for j in range(l1):
+                matrix[j][i] = 0
