dd98b4a73cab8c28879edb0bbe3db84052055d1c
金龙浩 <jinlha@jiedaibao.com>
2020-04-26 11:29:38 +0800

    情感丰富的文字

---
+++ normal/809.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def expressiveWords(self, S, words):
+        def RLE(S):
+            return zip(*[(k, len(list(grp)))
+                         for k, grp in itertools.groupby(S)])
+
+        R, count = RLE(S)
+        ans = 0
+        for word in words:
+            R2, count2 = RLE(word)
+            if R2 != R: continue
+            ans += all(c1 >= max(c2, 3) or c1 == c2
+                       for c1, c2 in zip(count, count2))
+
+        return ans
