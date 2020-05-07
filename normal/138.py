ec552dc17d719f9a26681c4f49ab60760237ef11
longhao54 <493319173@qq.com>
2020-05-07 22:00:48 +0800

    添加O1空间操作

--- normal/138.py
+++ normal/138.py
@@ -16,3 +16,37 @@ class Solution(object):
         node.next = self.copyRandomList(head.next)
         node.random = self.copyRandomList(head.random)
         return node
+
+
+
+class Solution(object):
+    def __init__(self):
+        self.visited = {}
+
+    def getClonedNode(self, node):
+        if node:
+            if node in self.visited:
+                return self.visited[node]
+            else:
+                self.visited[node] = Node(node.val, None, None)
+                return self.visited[node]
+        return None
+
+    def copyRandomList(self, head):
+        if not head:
+            return head
+
+        old_node = head
+        new_node = Node(old_node.val, None, None)
+        self.visited[old_node] = new_node
+
+        while old_node != None:
+
+            new_node.random = self.getClonedNode(old_node.random)
+            new_node.next = self.getClonedNode(old_node.next)
+
+            old_node = old_node.next
+            new_node = new_node.next
+
+        return self.visited[head]
+
