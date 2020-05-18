896921326fb229a7a8532a2733fd434f8baf5f05
金龙浩 <jinlha@jiedaibao.com>
2020-05-18 10:27:48 +0800

    最后一块石头的重量 II

--- normal/1049.py
+++ normal/1049.py
@@ -1,19 +1,13 @@
 class Solution:
-    def lastStoneWeightII(self, stones):
-        lenth = len(stones)
-        while lenth > 1:
-            stones.sort()
-            a = stones.pop() - stones.pop()
-            if a != 0:
-                stones.append(a)
-                lenth -= 1
-            else:
-                lenth -= 2
-        if lenth == 1:
-            return stones[0]
-        else:
-            return 0
+    def lastStoneWeightII(self, stones: List[int]) -> int:
 
-a = Solution()
-#print(a.lastStoneWeightII([2,7,4,1,8,1]))
-print(a.lastStoneWeightII([31,26,33,21,40]))
+        sums = sum(stones)
+        half = sums // 2
+
+        dp = [0] * (half + 1)
+
+        for stone in stones:
+            for j in range(len(dp) - 1, stone - 1, -1):
+                dp[j] = max(dp[j], dp[j - stone] + stone)
+
+        return sums - 2 * dp[half]
