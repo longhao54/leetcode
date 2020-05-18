9cd959fbc241e6fdcb490c394960a1934646c0a3
金龙浩 <jinlha@jiedaibao.com>
2020-05-18 10:29:46 +0800

    下一个排列

--- normal/31.py
+++ normal/31.py
@@ -1,13 +1,19 @@
+import bisect
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        lenth = len(nums)
-        last = nums[-1]
-        if 
-        for index, num in enumerate(nums[::-1]):
-            if num > last:
-                pass
-            last = num
+        index = len(nums) - 1
+        while index > 0:
+            if nums[index] > nums[index-1]:
+                break
+            index -= 1
         
+        if index > 0:
+            nums[index:] = sorted(nums[index:])
+            swap_index = bisect.bisect(nums, nums[index-1], lo=index)
+            nums[swap_index], nums[index-1] = nums[index-1], nums[swap_index]
+        else:
+            nums.reverse()
