d0d964d9631fc11479c4ec1c60c761c383160285
金龙浩 <jinlha@jiedaibao.com>
2020-05-22 11:14:12 +0800

    最大间距

---
+++ hard/164.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2: return 0
+        res = 0
+        nums.sort()
+        for i in range(1, n):
+            res = max(res, nums[i] - nums[i - 1])
+        return res
