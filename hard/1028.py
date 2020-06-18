fd2cb38ef5d81dbe3eb2e6a39145e4b79cf5749b
金龙浩 <jinlha@jiedaibao.com>
2020-06-18 10:09:26 +0800

    从先序遍历还原二叉树

---
+++ hard/1028.py
@@ -0,0 +1,21 @@
+class Solution:
+    def recoverFromPreorder(self, S: str) -> TreeNode:
+        path, pos = list(), 0
+        while pos < len(S):
+            level = 0
+            while S[pos] == '-':
+                level += 1
+                pos += 1
+            value = 0
+            while pos < len(S) and S[pos].isdigit():
+                value = value * 10 + (ord(S[pos]) - ord('0'))
+                pos += 1
+            node = TreeNode(value)
+            if level == len(path):
+                if path:
+                    path[-1].left = node
+            else:
+                path = path[:level]
+                path[-1].right = node
+            path.append(node)
+        return path[0]
