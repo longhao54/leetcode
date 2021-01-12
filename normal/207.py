6fde1f9f1cf991729d88a4bdb82c2d1f304e3fcf
金龙浩 <jinlha@jiedaibao.com>
2021-01-12 19:29:43 +0800

    拓扑排序

--- normal/207.py
+++ normal/207.py
@@ -21,3 +21,40 @@ class Solution:
                 if not indegrees[cur]: queue.append(cur)
         return not numCourses
 
+# 自己写的拓扑排序
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        todo = []
+        ans = defaultdict(int)
+        count = 0
+        sontof = defaultdict(list)
+        ftoson = defaultdict(list)
+
+        for i, j in prerequisites:
+            ftoson[j].append(i)
+            sontof[i].append(j)
+        
+        for k, v in sontof.items():
+            ans[k] = len(v)
+   
+        for i in range(numCourses):
+            if ans[i] == 0:
+                todo.append(i)
+                ans.pop(i)
+
+        while todo:
+            needtopop = []
+            count += 1
+            t = todo.pop()
+            for k in ftoson[t]:
+                ans[k] -= 1
+            for k, v in ans.items():
+                if v == 0:
+                    needtopop.append(k)
+                    todo.append(k)
+            for tmp in needtopop:
+                ans.pop(tmp)
+
+        return count == numCourses
